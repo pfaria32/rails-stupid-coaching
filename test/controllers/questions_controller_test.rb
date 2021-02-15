b2f6ee3aca001fa6c0fe3a83a6650215a628871d
Pedro Faria <pedrobentodefaria@gmail.com>
2021-02-15 18:10:12 -0300

    done

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -4,4 +4,12 @@ class QuestionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
 end
