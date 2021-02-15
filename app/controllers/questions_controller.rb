3758194c8ce1c1cb46a74c32eda2a474320ff382
Pedro Faria <pedrobentodefaria@gmail.com>
2021-02-15 17:39:09 -0300

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def ask
+    @answer = ""
+    @ask = params[:ask].to_s
+    @answer = coach_answer
+  end
+
+  def coach_answer
+    return "" if @ask == ''
+    return 'Silly question, get dressed and go to work!' if @ask.last == "?"
+    return "" if @ask.upcase == 'I AM GOING TO WORK RIGHT NOW!'
+    return 'I dont care, get dressed and go to work!'
+  end
+
+end
